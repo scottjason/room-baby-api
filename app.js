ea3d7a3de67adf0ba0943754c9a769f8b9d8bd94
Scott Jason <scottleejason@gmail.com>
2015-03-13 23:46:24 -0700

    adds connect-timeout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var timeout = require('connect-timeout');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -28,6 +29,13 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500).json(err || new Error('server error'));
 });
+
+app.use(timeout(120000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next){
+  if (!req.timedout) next();
+}
 
 app.listen(app.get('port'), function(){
   console.log('Room Baby Videos Api Listening on', app.get('port'));
