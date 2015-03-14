955d391e3af418e96c9b295650d65c6fd6ecd211
Scott Jason <scottleejason@gmail.com>
2015-03-14 01:21:31 -0700

    updates

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,29 @@
+var request = require('request');
+
+var sendVideo = function(obj, cb) {
+	var url = 'http://localhost:300/session/' + obj.sessionId + '/' + obj.archiveId + '/' + obj.size + '/' + obj.duration;
+	request.get(url, function(err, response, body){
+		console.log(err || response);
+		cb();
+	})
+}
+
 exports.ping = function(req, res, next) {
   res.render('index');
 };
 
 exports.videoStatus = function(req, res, next) {
   if(req.body.status === 'uploaded') {
-	 var sessionId = req.body.sessionId;
-	 var archiveId = req.body.id;	 
-	 var size = req.body.size;
-	 var duration = req.body.duration;
-	 console.log(res);
-	 res.redirect('http://localhost:300/session/' + sessionId + '/' + archiveId + '/' + size + '/' + duration);
+  	 var obj = {};
+	 obj.sessionId = req.body.sessionId;
+	 obj.archiveId = req.body.id;	 
+	 obj.size = req.body.size;
+	 obj.duration = req.body.duration;
+	 sendVideo(obj, function(){
+	   res.json();
+  	});
+
+	 // res.redirect('http://localhost:300/session/' + sessionId + '/' + archiveId + '/' + size + '/' + duration);
   }
   else {
   	res.json();
