eab82ec9d1ac4c411ec1ed72f3870df1b219fa92
Scott Jason <scottleejason@gmail.com>
2015-03-14 04:04:54 -0700

    adds video schema props

--- models/video.js
+++ models/video.js
@@ -1,5 +1,14 @@
 var mongoose = require('mongoose');
 
-var videoSchema = new mongoose.Schema({});
+var videoSchema = new mongoose.Schema({
+	status: String,
+	name: String,
+	sessionId: String,
+	partnerId: Number,
+	createdAt: Date,
+	size: Number,
+	duration: Number,
+	updatedAt: Number
+});
 
 module.exports = mongoose.model('Video', videoSchema);
