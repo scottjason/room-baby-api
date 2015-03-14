2b6652e425f7e7b59360654fffcccc999c599016
Scott Jason <scottleejason@gmail.com>
2015-03-14 00:49:24 -0700

    adds logs

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 var request = require('request');
 
 var sendVideo = function(videoObj) {
-  var opts = { host: 'http://localhost', port: 3000, path: '/session/video-ready', method: 'POST', body: videoObj }
+  console.log('sendVideo', videoObj);
+  var opts = { host: 'localhost', port: 3000, path: '/session/video-ready', method: 'POST', body: videoObj }
   request(opts, function(err, res, body){
   	if (!error && response.statusCode == 200) { 
   	  console.log('Success sending video obj', res.statusCode) 
@@ -15,12 +16,14 @@ exports.ping = function(req, res, next) {
 
 exports.getVideoStatus = function(req, res, next) {
   if(req.body.status === 'uploaded') {
+  	console.log(req.body.status);
 	var videoObj = {};
 	 videoObj.archiveId = req.body.id;
 	 videoObj.sessionId = req.body.sessionId;
 	 videoObj.size = req.body.size;
 	 videoObj.duration = req.body.duration;
 	 res.status(200).json();
+	 console.log('sent response');
 	 sendVideo(videoObj);
   }
   else {
