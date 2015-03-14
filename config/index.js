4e10baf6a206783174d5ac60e85602616018bef5
Scott Jason <scottleejason@gmail.com>
2015-03-14 13:12:35 -0700

    removes whitespace

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ if(!process.env.MONGOLAB_URI) env = require('../env');
 
 module.exports = {
   db: {
-  	uri: process.env.MONGOLAB_URI || env.MONGOLAB_URI,
+    uri: process.env.MONGOLAB_URI || env.MONGOLAB_URI,
     opts: {
       server: {
         socketOptions: { keepAlive: 1 }
