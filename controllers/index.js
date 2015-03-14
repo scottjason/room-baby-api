490953f18bcce3b24c184c314195318da2515f81
Scott Jason <scottleejason@gmail.com>
2015-03-13 23:18:54 -0700

    adds request cb to api

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,30 @@
+var request = require('request');
+
+var sendVideo = function(videoObj) {
+	var opts = {
+	 uri: "http://localhost:3000/session/video-ready",
+  	 method: "POST",
+  	 body: videoObj,
+  	 json: true
+  	}
+	
+  request(opts, function(err, res, body){
+	 console.log(err || res.statusCode);
+  });
+}
+
 exports.ping = function(req, res, next) {
   res.render('index');
 };
 
-exports.sendVideo = function(req, res, next) {
-  console.log('sendVideo req.body', req.body);
-  console.log('');
-  console.log('------------------');
-  console.log('');
-  console.log('sendVideo req', req);
+exports.getVideoStatus = function(req, res, next) {
+  if(req.body.status === 'uploaded') {
+	var videoObj = {};
+	 videoObj.archiveId = req.body.id;
+	 videoObj.sessionId = req.body.sessionId;
+	 videoObj.size = req.body.size;
+	 videoObj.duration = req.body.duration;
+	 res.status(200).end();
+	 sendVideo(videoObj);
+  }
 };
