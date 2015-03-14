a7fd8cbf00ee7662149d338dfb85664f37f34023
Scott Jason <scottleejason@gmail.com>
2015-03-13 22:10:38 -0700

    updates request type

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
-exports.index = function(req, res, next) {
+exports.ping = function(req, res, next) {
   res.render('index');
 };
 
 exports.sendVideo = function(req, res, next) {
-  console.log('sendVideo', req);
+  console.log('sendVideo req.body', req.body);
+  console.log('');
+  console.log('------------------');
+  console.log('');
+  console.log('sendVideo req', req);
 };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers');
 
-router.get('/', controller.index);
-router.get('/video-ready', controller.sendVideo);
+router.get('/', controller.ping);
+router.post('/video-ready', controller.sendVideo);
 
 module.exports = router;
