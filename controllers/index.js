ea78cf8505cc8b228e27d19ca0a530a6eb2760f8
Scott Jason <scottleejason@gmail.com>
2015-03-14 05:26:43 -0700

    updates url generator

--- controllers/index.js
+++ controllers/index.js
@@ -7,10 +7,11 @@ exports.ping = function(req, res, next) {
 exports.videoStatus = function(req, res, next) {
   if(req.body.status === 'uploaded') {
     var archiveId = req.body.id;
+    var partnerId = req.body.partnerId;
     delete req.body.id;
     var video = new Video(req.body);
     video.archiveId = archiveId;
-    video.url = video.generateUrl(video.parnterId, video.archiveId);
+    video.url = video.generateUrl(partnerId, archiveId);
   	video.save(function(err, savedVideo){
   	  console.log('saved video', savedVideo);
   	  	res.status(200).end();
