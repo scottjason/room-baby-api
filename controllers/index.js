a458f3d4c975c7ade515b3b4d4f60b4e79ae2473
Scott Jason <scottleejason@gmail.com>
2015-07-05 12:28:42 -0700

    logs siteUrl

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,7 @@ exports.render = function(req, res, next) {
 exports.generateVideo = function(req, res, next) {
   var videoUrl = 'https://s3-us-west-2.amazonaws.com/rtc-videos/45238782/e42810ef-7639-4fcd-aeb5-25c8833667c4/archive.mp4'
   var siteUrl = 'https://room-baby-video-api.herokuapp.com/' + req.body.partnerId + '/' + req.body.archiveId;
+  console.log('##### Site URL', siteUrl);
   var fbAppId = '921064881267563';
   res.locals.siteUrl = siteUrl;
   res.locals.videoUrl = videoUrl;
