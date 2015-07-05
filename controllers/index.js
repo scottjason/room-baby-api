bc62cae40a5b41cd113bb901b7a35a067fb557a1
Scott Jason <scottleejason@gmail.com>
2015-07-05 12:44:09 -0700

    adds back in ot endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,23 @@ exports.render = function(req, res, next) {
   res.render('index');
 };
 
+exports.videoStatus = function(req, res, next) {
+  if (req.body.status === 'uploaded') {
+    var archiveId = req.body.id;
+    var partnerId = req.body.partnerId;
+    delete req.body.id;
+    var video = new Video(req.body);
+    video.archiveId = archiveId;
+    video.url = video.generateUrl(partnerId, archiveId);
+    video.save(function(err, savedVideo) {
+      res.status(200).end();
+    });
+  } else {
+    res.status(200).end();
+  };
+};
+
+
 exports.generateVideo = function(req, res, next) {
   var videoUrl = 'https://s3-us-west-2.amazonaws.com/rtc-videos/45238782/e42810ef-7639-4fcd-aeb5-25c8833667c4/archive.mp4'
   var siteUrl = 'https://room-baby-video-api.herokuapp.com/' + req.body.partnerId + '/' + req.body.archiveId;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var controller = require('../controllers');
 
 router.get('/', controller.render);
+router.post('/video-status', controller.videoStatus);
 router.post('/embed', controller.generateVideo);
 
 module.exports = router;
