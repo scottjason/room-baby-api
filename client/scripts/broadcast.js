1cf46dc8312553db30fed414d55eb470dc152b9f
Scott Jason <scottleejason@gmail.com>
2015-07-19 15:04:48 -0700

    updates locals

--- client/scripts/broadcast.js
+++ client/scripts/broadcast.js
@@ -1,1 +1,8 @@
-console.log('broadcast init', broadcast);
+window.onload = function() {
+  var sessionId = document.getElementById('sessionId').innerHtml;
+  var token = document.getElementById('token').innerHtml;
+  var key = document.getElementById('key').innerHtml;
+  var secret = document.getElementById('secret').innerHtml;
+
+  console.log(sessionId, token, key, secret);
+}
